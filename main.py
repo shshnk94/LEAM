1a8e0970b600ff96390d8573b8e0213e1b56b23d
Shashanka Subrahmanya <shshnk94@gmail.com>
2019-10-06 01:13:40 -0700

    Max epochs 7

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Options(object):
         self.embed_size = 300
         self.lr = 1e-3
         self.batch_size = 256
-        self.max_epochs = 5
+        self.max_epochs = 7
         self.dropout = 0.5
         self.part_data = False
         self.portion = 1.0 
