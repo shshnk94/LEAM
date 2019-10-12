4e1658d41dad9328c1369173bf29994cf78a4e03
Shashanka Subrahmanya <shshnk94@gmail.com>
2019-10-11 19:47:30 -0700

    50 epochs

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Options(object):
         self.embed_size = 300
         self.lr = 0.01
         self.batch_size = 128
-        self.max_epochs = 20
+        self.max_epochs = 50
         self.dropout = 0.5
         self.part_data = False
         self.portion = 1.0 
